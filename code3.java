618c5d9c9b8bc8e8f0e9cd88b5610e3353b33bb5
pillireddy <pillisrinivasulureddy@gmail.com>
2025-04-22 04:13:49 +0000

    Java files Added

---
+++ code3.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class PalindromeCheck {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String str = sc.nextLine();
+        String reversed = new StringBuilder(str).reverse().toString();
+        if (str.equals(reversed)) {
+            System.out.println("The string is a palindrome.");
+        } else {
+            System.out.println("The string is not a palindrome.");
+        }
+    }
+}
+
